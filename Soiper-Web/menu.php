7d3e0cb9252bad1dee1deb191aa79872b761adb4
Mike <migsilvaz1@gmail.com>
2015-02-11 19:55:09 +0100

    estructura básica del menú definida

--- Soiper-Web/menu.php
+++ Soiper-Web/menu.php
@@ -7,6 +7,35 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		
+		<!-- Fixed navbar -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Soiper</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="#">Inicio</a></li>
+						<li><a href="#about">Noticias</a></li>
+						<li><a href="#contact">Contacto</a></li>
+						<li><a href="#contact">Sobre Nosotros</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="jumbotron pull-right" style="margin-top: 50px">
+					<a class="twitter-timeline" href="https://twitter.com/hayquemelol" data-widget-id="565581875084005376">Tweets por el @hayquemelol.</a>
+					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
