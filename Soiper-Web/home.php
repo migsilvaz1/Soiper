2b78a67eaf882cdfda4971dd2365add683cbcb52
Mike <migsilvaz1@gmail.com>
2015-02-12 13:33:27 +0100

    Commit de la muerte

---
+++ Soiper-Web/home.php
@@ -0,0 +1,97 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="jumbotron"></div>
+				<div class="jumbotron col-md-12">
+					<div class="row">
+						<div class="col-md-9">
+							<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+								<!-- Indicators -->
+								<ol class="carousel-indicators">
+									<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="5"></li>
+								</ol>
+
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner" role="listbox">
+									<div class="item active">
+										<div style="background-image: url(img/ejemplo1.jpg); height: 415px; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></div>
+										<div class="carousel-caption">
+											ejemplo1
+										</div>
+									</div>
+									<div class="item">
+										<div style="background-image: url(img/ejemplo2.jpg); height: 415px; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></div>
+										<div class="carousel-caption">
+											ejemplo2
+										</div>
+									</div>
+									<div class="item">
+										<div style="background-image: url(img/ejemplo3.jpg); height: 415px; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></div>
+										<div class="carousel-caption">
+											ejemplo3
+										</div>
+									</div>
+									<div class="item">
+										<div style="background-image: url(img/ejemplo4.jpeg); height: 415px; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></div>
+										<div class="carousel-caption">
+											ejemplo4
+										</div>
+									</div>
+									<div class="item">
+										<div style="background-image: url(img/ejemplo5.jpg); height: 415px; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></div>
+										<div class="carousel-caption">
+											ejemplo5
+										</div>
+									</div>
+									<div class="item">
+										<div style="background-image: url(img/ejemplo6.jpg); height: 415px; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></div>
+										<div class="carousel-caption">
+											ejemplo6
+										</div>
+									</div>
+								</div>
+
+								<!-- Controls -->
+								<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
+								<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<button href="#carousel-example-generic" data-slide-to="0" style="background-image: url(img/ejemplo1.jpg); height: 200px; width: 240px; border: none; background-position: center; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></button>
+						</div>
+						<div class="col-md-3">
+							<button href="#carousel-example-generic" data-slide-to="1" style="background-image: url(img/ejemplo2.jpg); height: 200px; width: 240px; border: none; background-position: center; background-repeat:no-repeat;background-size:cover; margin: 0px auto; margin-top: 15px"></button>
+						</div>
+					</div>
+					<div class="row" style="margin-top: 25px">
+						<div class="col-md-3">
+							<button href="#carousel-example-generic" data-slide-to="2" style="background-image: url(img/ejemplo3.jpg); height: 100px; width: 255px; border: none; background-position: center; background-repeat:no-repeat;background-size:cover; margin: 0px auto; margin-right: -15px;"></button>
+						</div>
+						<div class="col-md-3">
+							<button href="#carousel-example-generic" data-slide-to="3" style="background-image: url(img/ejemplo4.jpeg); height: 100px; width: 255px; border: none; background-position: center; background-repeat:no-repeat;background-size:cover; margin: 0px auto; margin-right: -15px;"></button>
+						</div>
+						<div class="col-md-3">
+							<button href="#carousel-example-generic" data-slide-to="4" style="background-image: url(img/ejemplo5.jpg); height: 100px; width: 240px; border: none; background-position: center; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></button>
+						</div>
+						<div class="col-md-3">
+							<button href="#carousel-example-generic" data-slide-to="5" style="background-image: url(img/ejemplo6.jpg); height: 100px; width: 240px; border: none; background-position: center; background-repeat:no-repeat;background-size:cover; margin: 0px auto;"></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
